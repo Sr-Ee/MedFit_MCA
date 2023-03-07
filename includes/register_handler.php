b52e773b9098fc6228c72c8ff48047afc4a7ce07
sunny <2022.sunny.halkatti@ves.ac.in>
2023-03-07 18:07:26 +0530

    committing changes

--- includes/register_handler.php
+++ includes/register_handler.php
@@ -4,7 +4,7 @@ $msg="";
 $error_array = array();
 if(isset($_POST['submit'])){
 
-    include 'C:\medfit1\MedFit_MCA\includes\db.php';  
+    include 'C:/xampp/htdocs//includes/db.php';  
    
     $_SESSION['is_register'] = true;
             
@@ -13,24 +13,29 @@ if(isset($_POST['submit'])){
     $fname = ucfirst(strtolower($fname)); //Capitalize first letter.
     $_SESSION['reg_fname'] = $fname; //Stores first name into the session
     
-    
-    $mname = strip_tags(mysqli_real_escape_string($conn,$_POST['middle_name'])); //Retrieve form values. //Remove HTML Tags
-    $mname = str_replace(' ','',$mname); //Space is replaced with nothing
-    $mname = ucfirst(strtolower($mname)); //Capitalize first letter.
-    $_SESSION['reg_mname'] = $mname; //Stores first name into the session
-
     $lname = strip_tags(mysqli_real_escape_string($conn,$_POST['last_name'])); //Retrieve form values. //Remove HTML Tags
     $lname = str_replace(' ','',$lname); //Space is replaced with nothing
     $lname = ucfirst(strtolower($lname)); //Capitalize first letter.
     $_SESSION['reg_lname'] = $lname; //Stores first name into the session.
 
 
+    $email = strip_tags(mysqli_real_escape_string($conn,$_POST['email']));
+    $_SESSION['reg_email'] = $email;
+
+    $user_gender = strip_tags(mysqli_real_escape_string($conn,$_POST['gender']));
+    $_SESSION['reg_gender'] = $user_gender;
+
+    $phone_num = strip_tags(mysqli_real_escape_string($conn,$_POST['phone']));
+    $_SESSION['reg_phone'] = $phone_num;
+
+    $age = strip_tags(mysqli_real_escape_string($conn,$_POST['age']));
+    $_SESSION['reg_age'] = $age;
+
     $user_role = strip_tags(mysqli_real_escape_string($conn,$_POST['role']));
     $_SESSION['reg_role'] = $user_role;
 
-    
-    $college_email = strip_tags(mysqli_real_escape_string($conn,$_POST['email']));
-    $_SESSION['reg_email'] = $college_email;
+    $address = strip_tags(mysqli_real_escape_string($conn,$_POST['address']));
+    $_SESSION['reg_address'] = $address;
    
     $password = strip_tags(mysqli_real_escape_string($conn,$_POST['password']));
     $_SESSION['reg_pass'] = $password;
@@ -38,8 +43,11 @@ if(isset($_POST['submit'])){
     $c_password = strip_tags(mysqli_real_escape_string($conn,$_POST['confirm_password']));
     $_SESSION['reg_cpass'] = $c_password;
 
-    $phone_num = strip_tags(mysqli_real_escape_string($conn,$_POST['phone']));
-    $_SESSION['reg_phone'] = $phone_num;
+    $clinic_address = strip_tags(mysqli_real_escape_string($conn,$_POST['clinic_address']));
+    $_SESSION['reg_clinic'] = $clinic_address;
+
+    $mrn = strip_tags(mysqli_real_escape_string($conn,$_POST['mrn']));
+    $_SESSION['reg_mrn'] = $mrn;
 
     //Date
     $signup_date = date("Y-m-d"); //Current date
@@ -54,23 +62,18 @@ if(isset($_POST['submit'])){
         array_push($error_array,"Your First Name must be between 2 and 25 characters <br>");
     }
     
-    if(strlen($mname) > 25 || strlen($mname) < 2)
-    {
-        array_push($error_array,"Your Middle Name must be between 2 and 25 characters <br>");
-    }
-
     if(strlen($lname) > 25 || strlen($lname) < 2)
     {
         array_push($error_array,"Your Last Name must be between 2 and 25 characters <br>");
     }
 
 
-    if($user_role === "student"){
+    if($user_role === "patient"){
 
-       if(preg_match("/^[a-z][a-zA-Z0-9$]+(@ascs.sies.edu.in)/ix",$college_email)){
+       if(preg_match("/^[a-z][a-zA-Z0-9$]+(@gmail.com)/ix",$email)){
        //if(preg_match("/^[a-z][a-zA-Z0-9$]+(@gmail.com)/ix",$college_email)){
 
-            $sql = "SELECT `COL_EMAIL` FROM `users` WHERE `COL_EMAIL` = '$college_email'";
+            $sql = "SELECT `email` FROM `patient` WHERE `email` = '$email'";
             $e_check = mysqli_query($conn,$sql);
 
             $num_rows = mysqli_num_rows($e_check);
@@ -87,13 +90,13 @@ if(isset($_POST['submit'])){
           array_push($error_array, "Invalid Format!<br>");
        }
 
-    }  //student bracket
+    }  //patient bracket
     else{
 
-        if(preg_match("/^[a-z][a-zA-Z0-9$]+(@sies.edu.in)/ix",$college_email)){
+        if(preg_match("/^[a-z][a-zA-Z0-9$]+(@gmail.com)/ix",$email)){
         //if(preg_match("/^[a-z][a-zA-Z0-9$]+(@gmail.com)/ix",$college_email)){
 
-            $sql = "SELECT `COL_EMAIL` FROM `users` WHERE `COL_EMAIL` = '$college_email'";
+            $sql = "SELECT `email` FROM `doctors` WHERE `email` = '$email'";
             $e_check = mysqli_query($conn,$sql);
 
             $num_rows = mysqli_num_rows($e_check);
@@ -138,43 +141,63 @@ if(isset($_POST['submit'])){
 
     if(empty($error_array)){
 
-        // $query = "SELECT `randSalt` FROM `users`";
-        // $select_randsalt_query = mysqli_query($conn,$query);
+        if($user_role === "patient"){
+            $password = md5($password); //encrypts the password before sending to database.
 
-        // if(!$select_randsalt_query){
+            $verification_id =  rand(111111111,999999999);
+            $query = "INSERT INTO `patient` (`first_name`, `last_name`, `email`, `gender`, `phone`, `age`, `password`, `address`, `dt`, `role`, `profile_pic`) VALUES ('$fname', '$lname', '$email', '$gender', '$phone', '$age', '$password', '$address', current_timestamp(), '$role', 'java.png');";
+            $register_query = mysqli_query($conn,$query);
+            if(!$register_query){
+                die("Query Failed" . mysqli_error($conn));
+            }
+            else{
+    
+                $msg = "<p style='color:green;'>We have just sent a verification link to <strong>{$email}</strong><br>Please checkyour inbox and click on the link
+                to get started. If you can't find this email, just request a new one here</p>";
+    
+                $mailHtml = "Please Confirm your account registration by clicking the button or link below: <a href='http://localhost/col_social_network/check.php?id={$verification_id}'>
+                http://localhost/col_social_network/check.php?id={$verification_id}</a>";
+    
+                smtp_mailer($email,'Account Verification',$mailHtml);
+            }
+    
+            //  //Reset Session
+            //  $_SESSION['reg_fname'] = "";
+            //  $_SESSION['reg_lname'] = "";
+            //  $_SESSION['reg_email'] = "";
+    
+            //  header("Location: register.php");
 
-        //     die("Query Failed" . mysqli_error($conn));
-        // }
-
-        // $row = mysqli_fetch_array($select_randsalt_query);
-        // $salt = $row['randSalt']; //Fetch this for encrypting password
-        // $password = crypt($password,$salt); //Encrypting Password
-
-        $password = md5($password); //encrypts the password before sending to database.
-
-        $verification_id =  rand(111111111,999999999);
-        $query = "INSERT INTO `users`(`FIRST_NAME`,`MIDDLE_NAME`,`LAST_NAME`,`COL_EMAIL`,`PASSWORD`,`SIGNUP_DATE`,`USER_ROLE`,`VERIFICATION_ID`,`VERIFY_STATUS`,`PHONE_NUM`,`PROFILE_PIC`) VALUES ('$fname','$mname','$lname','$college_email','$password','$signup_date','$user_role','$verification_id',0,'$phone_num','java.png')";
-        $register_query = mysqli_query($conn,$query);
-        if(!$register_query){
-            die("Query Failed" . mysqli_error($conn));
         }
         else{
+            $password = md5($password); //encrypts the password before sending to database.
 
-            $msg = "<p style='color:green;'>We have just sent a verification link to <strong>{$college_email}</strong><br>Please checkyour inbox and click on the link
-            to get started. If you can't find this email, just request a new one here</p>";
-
-            $mailHtml = "Please Confirm your account registration by clicking the button or link below: <a href='http://localhost/col_social_network/check.php?id={$verification_id}'>
-            http://localhost/col_social_network/check.php?id={$verification_id}</a>";
-
-            smtp_mailer($college_email,'Account Verification',$mailHtml);
+            $verification_id =  rand(111111111,999999999);
+            $query = "INSERT INTO `doctors` (`first_name`, `last_name`, `email`, `speciality`, `clinic_address`, `mrn`, `age`, `role`, `profile_pic`) VALUES ('$fname', '$lname', '$email', '$speciality', '$clinic_address', '$mrn', '$age', '$role', '$profile_pic');";
+            $register_query = mysqli_query($conn,$query);
+            if(!$register_query){
+                die("Query Failed" . mysqli_error($conn));
+            }
+            else{
+    
+                $msg = "<p style='color:green;'>We have just sent a verification link to <strong>{$email}</strong><br>Please checkyour inbox and click on the link
+                to get started. If you can't find this email, just request a new one here</p>";
+    
+                $mailHtml = "Please Confirm your account registration by clicking the button or link below: <a href='http://localhost/col_social_network/check.php?id={$verification_id}'>
+                http://localhost/col_social_network/check.php?id={$verification_id}</a>";
+    
+                smtp_mailer($email,'Account Verification',$mailHtml);
+            }
+    
+            //  //Reset Session
+            //  $_SESSION['reg_fname'] = "";
+            //  $_SESSION['reg_lname'] = "";
+            //  $_SESSION['reg_email'] = "";
+    
+            //  header("Location: register.php");
         }
 
-        //  //Reset Session
-        //  $_SESSION['reg_fname'] = "";
-        //  $_SESSION['reg_lname'] = "";
-        //  $_SESSION['reg_email'] = "";
-
-        //  header("Location: register.php");
+       
 
     }
 
@@ -186,21 +209,22 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-function smtp_mailer($to,$subject,$msg){
+function smtp_mailer($to,$subject,$msg)
+{
   try{
-    require 'C:/xampp/htdocs/colnet/php_mailer/vendor/autoload.php';
+    require 'C:/xampp/htdocs/MedFit_MCA/php_mailer/vendor/autoload.php';
     $mail = new PHPMailer(true);
     // $mail->SMTPDebug = 1;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'sonuhalkatti23@gmail.com';                     // SMTP username
-    $mail->Password   = 'Pandurang29@1971';                               // SMTP password
+    $mail->Username   = 'sunnyshmca04@gmail.com';                     // SMTP username
+    $mail->Password   = 'Srh28@2000';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('sonuhalkatti23@gmail.com');
+    $mail->setFrom('sunnyshmca04@gmail.com');
     $mail->addAddress($to);     // Add a recipient
 
     //$body = '<p><strong> Hello </strong> This is my first Email</p>';
--- register.php
+++ register.php
@@ -8,7 +8,7 @@
 <title>COLNET REGISTRATION</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/register_style.css"> <!-- register style sheet -->
+<link rel="stylesheet" href="./css/register_style.css"> <!-- register style sheet -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> 
